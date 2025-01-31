743c9c8895d1c4dabeccbd899c14cad8ecb52889
CarlyyyChen <huanyuechen@outlook.com>
2025-01-30 20:23:30 -0500

    complete arrObj

--- src/pages/qcomps/arrObj.tsx
+++ src/pages/qcomps/arrObj.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react';
-import { ItemListProps } from '../../types/arrObj';
+import { useState } from "react";
+import { ItemListProps } from "../../types/arrObj";
 
-let nextId = 3;
 const initialList = [
-  { id: 0, title: 'Big Bellies', seen: false },
-  { id: 1, title: 'Lunar Landscape', seen: false },
-  { id: 2, title: 'Terracotta Army', seen: true },
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Landscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
 ];
 
+/**
+ * The component renders two lists of artworks, each with a checkbox to mark the artwork as seen.
+ * However, the lists are coupled, that is, checking a box in one list will also check the box in the other list.
+ * Decouple the lists, that is, checking a box in one list should have no impact on the other list?
+ */
 export default function BucketList() {
   const [myList, setMyList] = useState(initialList);
-  const [yourList, setYourList] = useState(
-    initialList
-  );
 
+  const [yourList, setYourList] = useState(initialList);
+
+  /**
+   * The function updates the seen property of the artwork with the given id in the mylist.
+   * @param artworkId - the id of the artwork to toggle
+   * @param nextSeen - the value with which to update the seen property of the artwork
+   */
   function handleToggleMyList(artworkId: number, nextSeen: boolean) {
-    const tmpList = myList.map(e => {
-        if (e.id === artworkId) {
-            e.seen = nextSeen
-        }
-        return e
+    const tmpList = myList.map((e) => {
+      if (e.id === artworkId) {
+        return { ...e, seen: nextSeen }; // creating a shallow copy of the nested object
+      }
+      return e;
     });
     setMyList(tmpList);
   }
 
+  /**
+   * The function updates the seen property of the artwork with the given id in the yourlist.
+   * @param artworkId - the id of the artwork to toggle
+   * @param nextSeen - the value with which to update the seen property of the artwork
+   */
   function handleToggleYourList(artworkId: number, nextSeen: boolean) {
-    const tmpList = yourList.map(e => {
-        if (e.id === artworkId) {
-            e.seen = nextSeen
-        }
-        return e
+    const tmpList = yourList.map((e) => {
+      if (e.id === artworkId) {
+        return { ...e, seen: nextSeen };
+      }
+      return e;
     });
     setYourList(tmpList);
   }
@@ -38,13 +51,9 @@ export default function BucketList() {
     <>
       <h1>Art Bucket List</h1>
       <h2>My list of art to see:</h2>
-      <ItemList
-        artworks={myList}
-        onToggle={handleToggleMyList} />
+      <ItemList artworks={myList} onToggle={handleToggleMyList} />
       <h2>Your list of art to see:</h2>
-      <ItemList
-        artworks={yourList}
-        onToggle={handleToggleYourList} />
+      <ItemList artworks={yourList} onToggle={handleToggleYourList} />
     </>
   );
 }
@@ -52,17 +61,14 @@ export default function BucketList() {
 function ItemList({ artworks, onToggle }: ItemListProps) {
   return (
     <ul>
-      {artworks.map(artwork => (
+      {artworks.map((artwork) => (
         <li key={artwork.id}>
           <label>
             <input
               type="checkbox"
               checked={artwork.seen}
-              onChange={e => {
-                onToggle(
-                  artwork.id,
-                  e.target.checked
-                );
+              onChange={(e) => {
+                onToggle(artwork.id, e.target.checked);
               }}
             />
             {artwork.title}
